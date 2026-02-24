8ad21f19dffcfdad0392158bbc119a516de17d56
Giang <dollar_snowbell95@yahoo.com>
2026-02-24 20:27:30 +0200

    phonebook no entry

---
+++ CPP_00/ex01/classes.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+class Contact
+{
+	public:
+		std::string FirstName;
+		std::string	LastName;
+	private:
+		std::string	Nickname;
+		std::string	Number;
+		std::string	Secret;
+};
+
+class PhoneBook
+{
+	public:
+	
+};
---
+++ CPP_00/ex01/phonebook.cpp
@@ -0,0 +1,6 @@
+#include "classes.hpp"
+
+int main()
+{
+	
+}
