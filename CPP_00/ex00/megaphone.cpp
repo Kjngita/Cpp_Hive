b95ed064eed394422764aadcb0efbb593f2645d1
Giang <dollar_snowbell95@yahoo.com>
2026-02-24 19:41:00 +0200

    loud and clear

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	int i = 1;
+	while (av[i])
+	{
+		std::string line = av[i];
+		for (int j = 0; j < line.length(); j++)
+		{
+			line[j] = std::toupper((unsigned char)line[j]);
+		}
+		std::cout << line;
+		i++;
+	}
+	std::cout << std::endl;
+	return 0;
+}
