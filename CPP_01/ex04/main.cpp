b46bde3705414a7fb5a1ccde888765c1b89c310e
Giang <dollar_snowbell95@yahoo.com>
2026-03-12 21:41:18 +0200

    text is replaced

---
+++ CPP_01/ex04/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string	modify_text(std::string line, std::string target, std::string replacement)
+{
+	std::string	ret = line;
+	std::string::size_type	found = ret.find(target);
+	while (found != std::string::npos)
+	{
+		ret.erase(found, target.length());
+		ret.insert(found, replacement);
+		found = ret.find(target);
+	}
+	return (ret);
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 4)
+	{
+		std::cout << "Incorrect input. Please enter: <filename> str1 str2\n";
+		return 1;
+	}
+	std::ifstream	source(av[1]);
+	if (source.is_open())
+	{
+		std::string	name = av[1];
+		name = name.append(".replace");
+		std::ofstream	output(name.c_str());
+		std::string text;
+		while (getline(source, text))
+		{
+			text = modify_text(text, av[2], av[3]);
+			output << text << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << "Can't open file\n";
+		return 1;
+	}
+	return 0;
+}
