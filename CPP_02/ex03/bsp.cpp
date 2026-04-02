73257642178154783406ca58c150a05779156250
Giang <dollar_snowbell95@yahoo.com>
2026-04-02 19:21:31 +0300

    just go with this

--- CPP_02/ex03/bsp.cpp
+++ CPP_02/ex03/bsp.cpp
@@ -9,9 +9,9 @@ Fixed	area(const Point& a, const Point& b, const Point& c) {
 	Fixed cy = c.getY();
 
 	Fixed res = ax*(by-cy) + bx*(cy-ay) + cx*(ay-by);
-	if (res < 0)
+	if (res < Fixed(0))
 		res = Fixed(0) - res;
-	return (res);
+	return (res / 2);
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
@@ -19,8 +19,11 @@ bool bsp(Point const a, Point const b, Point const c, Point const point) {
 	Fixed	a1 = area(point, a, b);
 	Fixed	a2 = area(point, b, c);
 	Fixed	a3 = area(point, a, c);
-	std::cout << area_tri << " | " << a1 << " " << a2 << " " << a3 << std::endl;
 	if (a1.getRawBits() == 0 || a2.getRawBits() == 0 || a3.getRawBits() == 0)
 		return (false);
-	return (area_tri == a1 + a2 + a3);
+	Fixed	diff = area_tri - (a1 + a2 + a3);
+	if (diff >= 0 && diff < 0.01f)
+		return (true);
+	else
+		return false;
 }
--- CPP_02/ex03/main.cpp
+++ CPP_02/ex03/main.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-	Point A(0, 0);
-	Point B(20, 0);
-	Point C(0, 30);
-	Point P(1, 1);
+	Point A(0, -5.6);
+	Point B(20.63, 4.76);
+	Point C(0, 23.9);
+	Point P(0, 7.83);
 
 	if (bsp(A, B, C, P))
 		std::cout << "True!\n";
@@ -14,3 +14,9 @@ int main()
 		std::cout << "False!\n";
 	return 0;
 }
+
+/*
+https://www.desmos.com/calculator/bi14xz1jci
+For visualizing triangle. Add following equation to finish the triangle:
+y - y3 = s3(x - x3){x1 < x < x3}
+*/
