920aaa531871211d63931dcd2c49fcdc8e4aaa6a
Giang <dollar_snowbell95@yahoo.com>
2026-07-06 18:55:03 +0300

    pointer transformed

--- CPP_06/ex01/src/Serializer.cpp
+++ CPP_06/ex01/src/Serializer.cpp
@@ -1,9 +1,9 @@
 #include "Serializer.hpp"
 
 uintptr_t	Serializer::serialize(Data* ptr) {
-	//takes a pointer and converts to unsigned integer type uintptr_t
+	return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data*	Serializer::deserialize(uintptr_t raw) {
-	//takes an unsigned integer parameter and converts to a pointer to Data
+	return reinterpret_cast<Data*>(raw);
 }
--- CPP_06/ex01/src/main.cpp
+++ CPP_06/ex01/src/main.cpp
@@ -7,10 +7,18 @@ int main()
 	Data	barbie;
 	std::cout << barbie << std::endl;
 	barbie.setStr("Ah ah ah yeah");
-	std::cout << barbie << std::endl;
-	std::cout << "[ Memory address of OG data struct ] " << &barbie << std::endl;
+	std::cout << "[ Memory address of OG Data struct ]\t" << &barbie << std::endl;
 
-	// uintptr_t	ken = Serializer::serialize(&barbie);
+	std::cout << "\n\t===== Cast to uintptr_t =====\n";
+	uintptr_t	ken = Serializer::serialize(&barbie);
+	std::cout << "[ Value of uintptr ]\t\t" << ken << std::endl;
+	std::cout << "[ Memory address of uintptr_t ]\t" << &ken << std::endl;
+
+	std::cout << "\n\t===== Cast back to Data pointer =====\n";
+	Data*	aqua = Serializer::deserialize(ken);
+	std::cout << *aqua << std::endl;
+	std::cout << "[ Value of Data pointer ]\t\t" << aqua << " - Same with original struct address\n";
+	std::cout << "[ Memory address of Data pointer ]\t" << &aqua << std::endl;
 
 	return 0;
 }
